112e5e15566a035a197141dfdfc531421b67f0e5
laupwing <laupwing@gmail.com>
2025-10-21 09:27:35 +0200

    Add Bodyfillers page template for displaying content

---
+++ page-bodyfillers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Bodyfillers Page
+ * Template for displaying the Bodyfillers page
+ */
+
+get_header();
+?>
+
+<?php
+get_template_part('templates/page-banner', null, [
+    'title' => 'Bodyfillers'
+]);
+?>
+
+<?php get_footer(); ?>
